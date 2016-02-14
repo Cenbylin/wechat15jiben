bd6937367496870c8bef42ca5047245d8412d6e5
Cenbylin <827418637@qq.com>
2016-02-14 23:38:05 +0800

    核心信息分离成xml和读取对象

--- src/cn/cenbylin/po/InnerInfo.java
+++ src/cn/cenbylin/po/InnerInfo.java
@@ -1,5 +1,18 @@
 package cn.cenbylin.po;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -11,15 +24,18 @@ import com.google.gson.JsonParser;
  */
 public class InnerInfo {
 	private static String accessToken = null;
-	private static String appID = "wx2e7d71280147d5a5";
-	private static String appSecret = "bb54272fc561876a7c4f82d347f9c584";
+	private static String appID = null;
+	private static String appSecret = null;
 	// 七牛
-	private static String accessKey = "2hCizo1I0UNtRUMkr7Lztw18PO3cT48IjayfmDo9";
-	private static String secretKey = "a7qMw76FvqcgG8zEEHplzNxbr6uS9SnX6i4d1Ycq";
-	private static int Iterator = 100;//列举迭代器迭代大小
+	private static String accessKey = null;
+	private static String secretKey = null;
+	@SuppressWarnings("unused")
+	private static String bucket = null;
+	private static int Iterator;//列举迭代器迭代大小
 	static {
+		loadConfig();//加载
 		//定时任务线程
-		final long timeInterval = 1000;//时间
+		final long timeInterval = 100000;//时间(毫秒)
 		Runnable runnable = new Runnable() {
 			public void run() {
 				while (true) {
@@ -67,5 +83,52 @@ public class InnerInfo {
 	public static int getIterator() {
 		return Iterator;
 	}
+	@SuppressWarnings({ "unchecked", "unused" })
+	/**
+	 * 从配置文件中装载核心信息
+	 */
+	public static void loadConfig(){
+		Element root = null;
+		Map<String, String> map = new HashMap<String, String>();
+		SAXReader reader = new SAXReader();
+		try {
+				InputStream xmlStream = new FileInputStream(InnerInfo.class.getResource("").toString().replace("classes/cn/cenbylin/po/", "").replace("file:/", "")+"wechat-config.xml");
+				Document doc = reader.read(xmlStream);	
+				xmlStream.close();
+				root = doc.getRootElement();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (DocumentException e2){
+			
+		}
 
+		List<Element> list = root.elements();
+		for (Element e : list) {
+			if(e.getName().equals("wechat")){
+				List<Element> list1 = e.elements();
+				for(Element e1 : list1){
+					if(e1.getName().equals("appID")){
+						appID=e1.getStringValue();
+					}else if(e1.getName().equals("appSecret")){
+						appSecret=e1.getStringValue();
+					}
+				}
+			}else if(e.getName().equals("qiniu")){
+				List<Element> list1 = e.elements();
+				for(Element e1 : list1){
+					if(e1.getName().equals("accessKey")){
+						accessKey=e1.getStringValue();
+					}else if(e1.getName().equals("secretKey")){
+						secretKey=e1.getStringValue();
+					}else if(e1.getName().equals("bucket")){
+						bucket=e1.getStringValue();
+					}else if(e1.getName().equals("Iterator")){
+						Iterator=Integer.valueOf(e1.getStringValue());
+					}
+				}
+			}
+		}
+	}
 }
